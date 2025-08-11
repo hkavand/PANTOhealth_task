27e56eac784e6243421441479f42a53ac05bc188
Hossein Kavand <kavand.hossain@gmail.com>
2025-08-11 21:03:41 +0330

    Part 5 in progress, added Swagger API documentation.

--- src/xray/xray.controller.ts
+++ src/xray/xray.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Post, Put, Delete, Param, Body, Query } from '@nestjs/common';
 import { XrayService } from './xray.service';
+import { ApiTags, ApiOperation, ApiParam, ApiQuery, ApiBody } from '@nestjs/swagger';
 
+
+@ApiTags('Xrays')
 @Controller('xrays')
 export class XrayController {
     constructor(private readonly xrayService: XrayService) { }
-
 
     private validate(body: Record<string, { data: number[]; time: number }>) {
         if (!body || Object.keys(body).length === 0) {
@@ -24,11 +26,26 @@ export class XrayController {
     }
 
     @Get('filter')
+    @ApiOperation({ summary: 'Filter x-rays by deviceId and time' })
+    @ApiQuery({ name: 'deviceId', description: 'The device ID to filter by', required: false })
+    @ApiQuery({ name: 'time', description: 'The time to filter by', required: false, type: Number })
     async filterXrays(@Query('deviceId') deviceId: string, @Query('time') time: number) {
         return this.xrayService.filterXrays(deviceId, time);
     }
 
     @Post()
+    @ApiOperation({ summary: 'Create a new x-ray' })
+    @ApiBody({
+        description: 'The x-ray data to create',
+        schema: {
+            example: {
+                "66bb584d4ae73e488c30a072": {
+                    "data": [1, 2, 3, 4],
+                    "time": 1735683480000
+                }
+            }
+        },
+    })
     async createXray(@Body() body: Record<string, { data: number[]; time: number }>) {
         const { deviceId, time, dataLength } = this.validate(body);
 
@@ -36,16 +53,32 @@ export class XrayController {
     }
 
     @Get()
+    @ApiOperation({ summary: 'Get all x-rays' })
     async getAllXrays() {
         return this.xrayService.getAllXrays();
     }
 
     @Get(':id')
+    @ApiOperation({ summary: 'Get an x-ray by ID' })
+    @ApiParam({ name: 'id', description: 'The ID of the x-ray', type: String })
     async getXrayById(@Param('id') id: string) {
         return this.xrayService.getXrayById(id);
     }
 
     @Put(':id')
+    @ApiOperation({ summary: 'Update an x-ray by ID' })
+    @ApiParam({ name: 'id', description: 'The ID of the x-ray', type: String })
+    @ApiBody({
+        description: 'The x-ray data to update',
+        schema: {
+            example: {
+                "66bb584d4ae73e488c30a072": {
+                    "data": [1, 2, 3, 4],
+                    "time": 1735683480000
+                }
+            }
+        },
+    })
     async updateXray(
         @Param('id') id: string,
         @Body() body: Record<string, { data: number[]; time: number }>,
@@ -65,6 +98,8 @@ export class XrayController {
     }
 
     @Delete(':id')
+    @ApiOperation({ summary: 'Delete an x-ray by ID' })
+    @ApiParam({ name: 'id', description: 'The ID of the x-ray', type: String })
     async deleteXray(@Param('id') id: string) {
         return this.xrayService.deleteXray(id);
     }
--- xray-producer/src/app.controller.ts
+++ xray-producer/src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('X-Ray Producer')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get('send-xray')
+  @ApiOperation({ summary: 'Send sample x-ray data to RabbitMQ' })
   sendXrayData() {
     this.appService.sendXrayData();
     return { message: 'X-ray data sent successfully' };
